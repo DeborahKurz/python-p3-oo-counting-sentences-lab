8f696dee01838613695799475fd051d3189e1915
Deborah Kurz <DeborahKurz6@gmail.com>
2024-04-04 11:14:00 -0600

    Value checked

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=''):
+    self._value = value
+
+  @property
+  def value(self):
+    return self._value
+
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print("The value must be a string.")
